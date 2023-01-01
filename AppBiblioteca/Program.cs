912619456c408a732b7ad72121e9fcb6038fefee
Jorge_Zeros <161534@unsaac.edu.pe>
2023-01-01 13:25:32 -0500

    Crear proyectos de aplicación

---
+++ AppBiblioteca/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ EstDatos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EstDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
