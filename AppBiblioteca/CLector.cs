5cc8bbdf438f950aea13ae1e1b0fc16fce979b89
saul <jhonesau2003@gmail.com>
2023-01-04 15:55:06 -0500

    Nuevos Cambios

---
+++ AppBiblioteca/CLector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstDatos
+{
+    class CLector
+    {
+        public string idLector;
+        public string apellidos;
+        public string nombres;
+        public string direccion;
+        public string sexo;
+        public string edad;
+
+        public CLector(string PidLector, string Papellidos, string Pnombres, string Pdireccion, string Psexo, string Pedad)
+        {
+            idLector = PidLector;
+            apellidos = Papellidos;
+            nombres = Pnombres;
+            direccion = Pdireccion;
+            sexo = Psexo;
+            edad = Pedad;
+        }
+    }
+}
---
+++ AppBiblioteca/CTesis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstDatos
+{
+    class CTesis
+    {
+        public string idTesis;
+        public string titulo;
+        public string autor;
+        public string anio;
+        public string area;
+        public string asesor;
+
+        public CTesis(string PidTesis, string Ptitulo, string Pautor, string Panio,string Parea, string Pasesor)
+        {
+            idTesis = PidTesis;
+            titulo = Ptitulo;
+            autor = Pautor;
+            anio = Panio;
+            area = Parea;
+            asesor = Pasesor;
+        }
+
+
+    }
+}
